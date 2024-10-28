56344f8b51d3b48f59246e8ec209a4aecaa3dc1c
Bruno Pavan Adario Moreira <bpmoreira@TOPAZ.CORP>
2024-10-28 13:44:42 -0300

    feat(cloud-parking): add/treatment connection to database/insert jpa repository

--- src/main/java/br/com/bruno/parking/model/Parking.java
+++ src/main/java/br/com/bruno/parking/model/Parking.java
@@ -1,20 +1,34 @@
 package br.com.bruno.parking.model;
 
 import br.com.bruno.parking.controller.dto.ParkingDTO;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@Entity
 public class Parking {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+
     private String license;
+
     private String state;
+
     private String model;
+
     private String color;
+
     private LocalDateTime entryDate;
+
     private LocalDateTime exitDate;
+
     private Double bill;
 
     public Parking() { }
